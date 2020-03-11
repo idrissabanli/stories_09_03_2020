faeff1d6ea816a077d69a302c6d7ef8518bdab3c
idris <idris.sabanli@gmail.com>
2020-03-12 00:31:39 +0400

    jinja

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/tnabiev000/PythonProjects/project/project/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- app/views.py
+++ app/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def index(request):
-    return render(request , 'index.html' )
+    # mystring = 'Teymur'
+    # birthdate = datetime.datetime.now()
+    # html = "<h1>Nebiyev</h1>"
+    # a = [75,2,3,4,5,6]
+
+    # context = {
+    #     'name': mystring,
+    #     'birthdate': birthdate,
+    #     'html': html,
+    #     'array': a,
+    # }
+
+    mypost = [
+        {
+            'title': 'Tasty & Delicious Foods1',
+            'category': 'Dessert',
+            'date': datetime.date(2017,3,3),
+            'image': 'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg'
+        }, 
+        {
+            'title': 'Tasty & Delicious Foods2',
+            'category': 'Dessert',
+            'date': datetime.date(2017,3,3),
+            'image': 'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg'
+        }, 
+        {
+            'title': 'Tasty & Delicious Foods3',
+            'category': 'Dessert',
+            'date': datetime.date(2017,3,3),
+            'image': 'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg'
+        }, 
+        {
+            'title': 'Tasty & Delicious Foods4',
+            'category': 'Dessert',
+            'date': datetime.date(2017,3,3),
+            'image': 'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg'
+        },
+    ]
+
+    context = {
+        'posts': mypost
+    }
+    return render(request , 'index.html', context)
 
 def about(request):
     return render(request , 'about.html' )
